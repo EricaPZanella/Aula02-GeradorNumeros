b981fd8564aeeab8a0e7ce7f2454eff29c37476a
Erica <ericapzanella@gmail.com>
2019-02-22 21:22:49 -0300

    Aula2 - exemplos

--- app/src/main/java/br/edu/ifsc/pdm/geradornumeroaleatorio/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/pdm/geradornumeroaleatorio/MainActivity.java
@@ -2,6 +2,10 @@ package br.edu.ifsc.pdm.geradornumeroaleatorio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void alterarTexto(View view) {
+
+        //Button b = (Button) view;
+        //b.setText("Aula de sexta é boa!");
+
+        TextView text = (TextView) findViewById(R.id.text);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        text.setText(editText.getText().toString());
+
+    }
+
 }
